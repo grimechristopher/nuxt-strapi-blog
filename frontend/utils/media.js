c399b36465a0c35e46092754fc70e1f4185118eb
Christopher Grime <54651065+grimechristopher@users.noreply.github.com>
2023-05-19 11:15:02 -0700

    Remove unused example code

--- frontend/utils/media.js
+++ frontend/utils/media.js
@@ -5,7 +5,3 @@ export function getStrapiMedia(url) {
   }
   return config.strapiBaseUri ? `${config.strapiBaseUri}/${url}` : `http://127.0.0.1:1337/${url}`;;
 }
-
-export const useFoo = () => {
-  return useState('foo', () => 'bar')
-}
